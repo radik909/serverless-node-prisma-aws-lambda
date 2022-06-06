2420433747968faf5b5554026007f4ea6f39e9df
ramand <dineshkumar.raman@umusic.com>
2022-06-06 13:24:47 +0530

    Add prisma ORM

--- serverless.ts
+++ serverless.ts
@@ -20,7 +20,15 @@ const serverlessConfiguration: AWS = {
   },
   // import the function via paths
   functions: { hello },
-  package: { individually: true },
+  package: {
+    individually: true,
+    patterns: [
+      '!node_modules/.prisma/client/libquery_engine-*',
+      'node_modules/.prisma/client/libquery_engine-rhel-*',
+      '!node_modules/prisma/libquery_engine-*',
+      '!node_modules/@prisma/engines/**',
+    ],
+  },
   custom: {
     esbuild: {
       bundle: true,
