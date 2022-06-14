5edd61552dd468bcae14d12bb3a9b5691772d183
ramand <dineshkumar.raman@umusic.com>
2022-06-14 13:31:17 +0530

    Configure dotenv plugin

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import hello from '@functions/hello';
 const serverlessConfiguration: AWS = {
   service: 'chaos',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild', 'serverless-offline'],
+  plugins: ['serverless-dotenv-plugin', 'serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
